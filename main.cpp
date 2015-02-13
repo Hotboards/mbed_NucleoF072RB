8c11cf1bff28317ccc177b189aa41c53446dc0e6
Diego <diego.perez@hotboards.org>
2015-02-13 00:22:40 -0700

    added mbed rtos

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 /**----------------------------------------------------------------------------
  * @target      Nucleo F072RB
- * @libraries   mbed drivers only
+ * @libraries   mbed drivers and rtos
  * @date        12-February-2015
  * @author      Hotboards
  * @brief       This is a project build with the mbed libraries ready to compile
- * using GNU ARM compiler and eclipse, it has only the driver libraries at the
- * moment. Should be use as a template only copy and paste to generate a new
- * independent project, or use egit to clone from the github repository.
- * in folder Debug you will find .hex, .elf and .bin file to program your board
- * check the readme file for more information
+ * using GNU ARM compiler and eclipse, it has only the driver libraries and the
+ * rtx OS at the moment. Should be use as a template only copy and paste to
+ * generate a new independent project, or use egit to clone from the github
+ * repository. In folder Debug you will find .hex, .elf and .bin file to program
+ * your board check the readme file for more information
 -----------------------------------------------------------------------------*/
 #include "mbed.h"
 
